5635c98eb82f2564c48e2c87261d14d5ae8c064b
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-01-28 22:44:17 +0530

    data types

---
+++ 006-primitive.js
@@ -0,0 +1,20 @@
+// nn ss bb u
+
+let a = null
+let b = 256
+let c = "Hello Boss"
+let d = Symbol("This is symbol")
+let e = true
+let f = BigInt(12555)
+let g 
+
+console.log(` a : ${a} and type of a : ${typeof a}`)
+console.log(` b : ${b} and type of b : ${typeof b}`)
+console.log(` c : ${c} and type of c : ${typeof c}`)
+
+// console.log(` d : ${d} and type of d : ${typeof d}`)
+ console.log(" d :"  ,  d  , "and type of d : " , typeof d)
+
+console.log(` e : ${e} and type of e : ${typeof e}`)
+console.log(` f : ${f} and type of f : ${typeof f}`)
+console.log(` g : ${g} and type of f : ${typeof f}`)
---
+++ 007-object.js
@@ -0,0 +1,15 @@
+// This is non primitive data type
+
+const obj ={
+   "A" : true ,
+   "B" : false ,
+   "num" : 2000,
+   "fun" : undefined,
+   "name": "Piyush Dahle"
+}
+
+console.log(obj["A"])
+console.log(obj["B"])
+console.log(obj["num"])
+console.log(obj["fun"])
+console.log(obj["name"])
