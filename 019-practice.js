fdc5a6cae137295679cdd3f57122eb03ad392387
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-01-29 22:36:02 +0530

    object and loop marks que

--- 019-practice.js
+++ 019-practice.js
@@ -1,15 +1,16 @@
-const marks={
-    "Piyush": 80,
-    "Akshay": 95,
-    "Ram":70,
-    "Rahul":60
-}
+const marks = {
+  Piyush: 80,
+  Akshay: 95,
+  Ram: 70,
+  Rahul: 60,
+};
 
 for (let i = 0; i < Object.keys(marks).length; i++) {
-    console.log(` The marks of ${Object.keys(marks)[i]} is ${marks[Object.keys(marks)[i]]}`);
+  console.log(
+    ` The marks of ${Object.keys(marks)[i]} is ${marks[Object.keys(marks)[i]]}`
+  );
 }
 
-for(let key in marks)
-{
-    console.log(`The marks of ${key} are  ${marks[key]}`);
+for (let key in marks) {
+  console.log(`The marks of ${key} are ${marks[key]}`);
 }
