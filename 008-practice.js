a23d92c86d975f11913f160b8cfbbbd75da022a1
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-01-29 15:55:13 +0530

    Practice code

---
+++ 008-practice.js
@@ -0,0 +1,21 @@
+// Create a variable type string and try to add a number to it
+
+var a = "10"
+var b = "20"
+var c = 30
+var d = 40
+
+console.log(`${a+c}`) //ans
+
+//extra
+
+console.log(`${a+b}`) 
+console.log(`${c+b}`) 
+console.log(`${c+d}`) 
+
+// use type of operator to find the datatype of the string in the last question
+
+console.log(`${typeof a}`) 
+console.log(`${typeof c}`) 
+console.log(`${typeof c+a}`) 
+console.log(`${typeof a+c}`) 
---
+++ 009-practice.js
@@ -0,0 +1,17 @@
+const obj = {
+    name : "Piyush Dahle",
+    age: 23,
+    city:"Bhopal",
+    country:"India"
+}
+console.log(obj["country"])
+
+console.log(obj)
+
+// adding object item
+obj["Father Name"]= "Munna Lal Dahle";
+
+// rename the object items
+obj["city"]="Balaghat";
+
+console.log(obj)
