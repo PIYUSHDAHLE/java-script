1cd3e3ddfff45bd64631b135c3f737a00a96632b
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-01-30 22:42:44 +0530

    basic of Array

---
+++ 027-array.js
@@ -0,0 +1,14 @@
+let marks =[65,85,95,44,77,null,undefined]
+
+console.log(marks)
+console.log(marks[0])
+console.log(marks[1])
+console.log(marks[2])
+console.log(marks[3])
+
+console.log("Length of array :",marks.length)
+
+marks[4] = 87; // array is mutable but string is immutable 
+console.log(marks)
+
+console.log(typeof marks) //object
