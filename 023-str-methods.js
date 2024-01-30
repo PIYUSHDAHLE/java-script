809b3c3e17109b3529344f760aa8b20ce33b9eb3
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-01-30 19:25:52 +0530

    string methods

---
+++ 023-str-methods.js
@@ -0,0 +1,16 @@
+let movie = "Salaar_Animal_Fighter"
+let hero = "     Munna Lal Dahle          "
+
+console.log(movie.slice(4))
+console.log(movie.slice(0))
+console.log(movie.slice(2,7))
+
+console.log(movie.toUpperCase())
+console.log(movie.toLowerCase())
+
+console.log(movie.replace("Salaar","Jai_Shree_Ram"))
+
+console.log(hero.concat(movie))
+
+console.log(hero)
+console.log(hero.trim())
