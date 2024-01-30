4d96b5a0682eb85ae15984a5789b47d673d33534
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-01-30 15:55:33 +0530

    basics of string

---
+++ 022-string.js
@@ -0,0 +1,18 @@
+let nam = "PIYUSH";
+console.log(nam);
+console.log(nam.length);
+console.log(nam[0]);
+console.log(nam[1]);
+console.log(nam[2]);
+console.log(nam[3]);
+console.log(nam[4]);
+
+//template literals
+let b1 ='Ram';
+let b2='Shyam';
+
+
+console.log(`${b1} and ${b2} both are brothers.`);
+
+console.log("brother's")
+console.log("brother\"s")
