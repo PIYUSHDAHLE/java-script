9217bc071171539ff1a53c6fb3c25122ad077c4d
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-02-02 23:14:40 +0530

    Practice code

---
+++ 042-practice.js/script.js
@@ -0,0 +1,8 @@
+let age = Number.parseInt(prompt("Enter the age"));
+
+if(age>=18)
+{
+    document.write("you are eligible to drive");
+}else{   
+    document.write("you are not eligible to drive");
+}
