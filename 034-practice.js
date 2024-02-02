5308fede8dd68b20b9f2d1da268c29ecdf74314b
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-02-02 18:37:28 +0530

    using push method

--- 034-practice.js
+++ 034-practice.js
@@ -1,12 +1,12 @@
 //Keep add the number into array into 1 until we not enter the zero number and show that array
-const prompt =  require('prompt-sync')();
+const prompt = require("prompt-sync")();
 
 let arr = [];
 let n;
 
-do {    
-    n =  Number.parseInt(prompt("Enter the number : "));
-    arr.push(n);
-}while(n!=0)
+do {
+  n = Number.parseInt(prompt("Enter the number : "));
+  arr.push(n);
+} while (n != 0);
 
 console.log(arr);
