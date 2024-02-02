0a7fe08a0c968b686bff985594c8a375bad1e79d
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-02-02 23:06:17 +0530

    some commands in js

--- 041-more-command/script.js
+++ 041-more-command/script.js
@@ -5,7 +5,17 @@ let a = Number.parseInt(prompt("Enter the number","546"));
 let write = confirm("DO YOU WANT TO WRITE SOME THING ?");
 
 if(write){
-    document.write(a)
+
+    document.write("The value you entered : ",a)
 }else{
     document.write("plzz allow me write")
 }
+
+let c = prompt("Enter the background color you want :")
+
+document.body.style.background=c;
+
+window.console.log(window);
+console.log(window);
+
+console.log(document);
