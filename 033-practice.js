a2c94645a43f4fd8c256046a878fdf86781fda03
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-02-02 15:33:37 +0530

    Add the num in array

--- 033-practice.js
+++ 033-practice.js
@@ -3,12 +3,8 @@
 const prompt = require("prompt-sync")();
 let arr=[11,22,33];
 
-let n=Number.parseInt(prompt("Enter the number : "))
+let n=Number.parseInt(prompt("Enter the number : "));
 
-arr.push(n)
+arr.push(n);
 
 console.log(arr);
-
-
-
-
