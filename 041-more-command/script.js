51da2c333cf24677b9d99a197eadaa5000bdb211
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-02-02 22:49:41 +0530

    some commands in js

---
+++ 041-more-command/script.js
@@ -0,0 +1,11 @@
+alert("Enter the value of a");
+let a = Number.parseInt(prompt("Enter the number","546"));
+
+
+let write = confirm("DO YOU WANT TO WRITE SOME THING ?");
+
+if(write){
+    document.write(a)
+}else{
+    document.write("plzz allow me write")
+}
