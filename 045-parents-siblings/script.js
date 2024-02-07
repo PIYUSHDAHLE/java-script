fdc2085606a3f13072bf4f39cefac0eafe603103
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-02-07 15:49:18 +0530

    parents siblings

---
+++ 045-parents-siblings/script.js
@@ -0,0 +1,8 @@
+//Parent
+console.log(document.body.parentNode)
+console.log(document.body.parentElement) 
+console.log(document.documentElement.parentElement) //null
+
+//Sibling
+console.log(document.body.nextSibling) ///null
+console.log(document.body.previousSibling)
